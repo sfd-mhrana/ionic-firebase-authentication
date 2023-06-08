626586df35d641407c8c1ea9be242b2fcd9d26b4
sfd-mhrana <sfd.mhrana@gmail.com>
2023-06-08 22:18:21 +0600

    Firebase Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAgQQ_oCq3ul6rs5OuAIMFfKDkxPGtDhxE",
+    authDomain: "ionicfirebaseauth-ecc94.firebaseapp.com",
+    projectId: "ionicfirebaseauth-ecc94",
+    storageBucket: "ionicfirebaseauth-ecc94.appspot.com",
+    messagingSenderId: "316424924144",
+    appId: "1:316424924144:web:63acabcdc854dbff388b35",
+    measurementId: "G-T8Y8VTQKR0"
+  }
 };
